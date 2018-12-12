f0de5605539e739c2386b5a0de4ae56cf0152a89
UdjinSkobelev <udjinskobelev@gmail.com>
2018-12-12 19:44:33 +0300

    #1 project initialization

--- src/main/java/org/webtree/auth/domain/WTUserDetails.java
+++
@@ -1,9 +0,0 @@
-package org.webtree.auth.domain;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface WTUserDetails<T> extends UserDetails {
-    T getId();
-
-    String getUsername();
-}
