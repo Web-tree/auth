06728216bba444c675df26eb273a7a04dc206538
UdjinSkobelev <udjinskobelev@gmail.com>
2018-12-24 20:48:51 +0300

    #2 service test refactor

--- core/src/test/java/org/webtree/auth/service/AuthenticationServiceTest.java
+++ core/src/test/java/org/webtree/auth/service/AuthenticationServiceTest.java
@@ -3,11 +3,12 @@ package org.webtree.auth.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
 import org.mockito.Mock;
+import org.mockito.internal.creation.MockSettingsImpl;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.webtree.auth.domain.AuthDetails;
-import org.webtree.auth.domain.Token;
+
 import org.webtree.auth.domain.WtUserDetails;
 import org.webtree.auth.repository.AuthRepository;
 
@@ -16,6 +17,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthenticationServiceTest {
@@ -25,12 +28,12 @@ public class AuthenticationServiceTest {
     private AuthRepository repository;
     @Mock
     private WtUserDetails details;
-
     private AbstractAuthenticationService service;
 
     @BeforeEach
-    public void setUp() throws Exception {
-        service = createService();
+    void setUp() {
+        service = mock(AbstractAuthenticationService.class,
+                withSettings().defaultAnswer(Answers.CALLS_REAL_METHODS).useConstructor(repository));
     }
 
     @Test
@@ -44,19 +47,5 @@ public class AuthenticationServiceTest {
         given(repository.findByUsername(TEST_USERNAME)).willReturn(Optional.empty());
         assertThatThrownBy(() -> service.loadUserByUsername(TEST_USERNAME))
                 .isInstanceOf(UsernameNotFoundException.class);
-    }
-
-    private AbstractAuthenticationService createService() {
-        return new AbstractAuthenticationService(repository) {
-            @Override
-            public WtUserDetails register(AuthDetails userDetails) {
-                return null;
-            }
-
-            @Override
-            public Token login(AuthDetails userDetails) {
-                return null;
-            }
-        };
     }
 }
