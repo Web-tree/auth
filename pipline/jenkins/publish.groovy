6862ff6cf20f9020e3bbe86ed0598874c1600bd4
Max Levitskiy <max.dstu@gmail.com>
2019-03-23 21:46:00 +0100

    Migrate publish pipeline to new jenkins #21

--- pipline/jenkins/publish.groovy
+++ pipline/jenkins/publish.groovy
@@ -1,12 +1,17 @@
 library identifier: 'webtree-lib@master', retriever: modernSCM(
-        [$class: 'GitSCMSource',
-         remote: 'https://github.com/Web-tree/jenkins.git'])
+    [$class: 'GitSCMSource',
+     remote: 'https://github.com/Web-tree/jenkins.git'])
 
 pipeline {
     agent {
-        docker {
-            image 'maven:3-jdk-8-alpine'
-            args '-v jenkins_m2:/root/.m2'
+        kubernetes {
+            label 'auth-publish-maven'
+            containerTemplate {
+                name 'maven'
+                image 'maven:3.3.9-jdk-8-alpine'
+                ttyEnabled true
+                command 'cat'
+            }
         }
     }
 
@@ -32,5 +37,4 @@ pipeline {
             slackSend(color: '#FF0000', message: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
         }
     }
-
 }
