56384b859cdccc1784ab2c20459c2ee7f75ae27e
Max Levitskiy <max.dstu@gmail.com>
2019-05-12 18:55:25 +0200

    Reverted package rename, remove checkstyle

---
+++ auth-core/src/main/java/org/webtree/auth/domain/AuthDetailsImpl.java
@@ -0,0 +1,62 @@
+package org.webtree.auth.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.webtree.auth.deserializer.EncodePasswordDeserializer;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AuthDetailsImpl implements Serializable, AuthDetails {
+    private static final long serialVersionUID = 6425650941625914226L;
+    private String username;
+    private String password;
+
+    public AuthDetailsImpl(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public AuthDetailsImpl() {
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @JsonDeserialize(using = EncodePasswordDeserializer.class)
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AuthDetailsImpl that = (AuthDetailsImpl) o;
+        return Objects.equals(username, that.username) &&
+                Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+
+    @Override
+    public String toString() {
+        return "AuthDetailsImpl{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ auth-core/src/main/java/org/webtree/auth/repository/AuthRepositoryImpl.java
@@ -0,0 +1,44 @@
+package org.webtree.auth.repository;
+
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.query.Query;
+import org.springframework.stereotype.Repository;
+import org.webtree.auth.domain.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.springframework.data.cassandra.core.query.Criteria.where;
+
+
+
+@Repository
+public class AuthRepositoryImpl implements AuthRepository {
+    private CassandraOperations operations;
+
+    @Autowired
+    public AuthRepositoryImpl(CassandraOperations operations) {
+        this.operations = operations;
+    }
+
+    @Override
+    public Optional<User> findByUsername(String name) {
+        return Optional.ofNullable(operations.selectOne(Query.query(where("username").is(name)), User.class));
+    }
+
+    @Override
+    public User saveIfNotExists(User user) {
+        operations.getCqlOperations()
+                .execute(QueryBuilder
+                        .insertInto(operations.getTableName(user.getClass()).getUnquoted())
+                        .value("id", UUID.randomUUID().toString())
+                        .value("password", user.getPassword())
+                        .value("username", user.getUsername())
+                        .ifNotExists());
+
+
+        return user;
+    }
+}
