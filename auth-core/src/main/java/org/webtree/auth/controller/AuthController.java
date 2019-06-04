6e26b3c45bfaca97ecbaabfb211d8b1790c6aa13
Max Levitskiy <max.dstu@gmail.com>
2019-06-04 09:42:20 +0200

    Remove AuthDetailsRequestBodyProcessor

--- auth-core/src/main/java/org/webtree/auth/controller/AuthController.java
+++ auth-core/src/main/java/org/webtree/auth/controller/AuthController.java
@@ -3,7 +3,12 @@ package org.webtree.auth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.webtree.auth.domain.AuthDetails;
 import org.webtree.auth.domain.Token;
 import org.webtree.auth.domain.User;
@@ -22,13 +27,13 @@ public class AuthController {
     }
 
     @PostMapping("#{AuthPropertiesBean.route.register}")
-    public ResponseEntity<?> register(AuthDetails authDetails) {
+    public ResponseEntity<?> register(@RequestBody AuthDetails authDetails) {
         return service.registerIfNotExists(authDetails) ?
                 ResponseEntity.status(HttpStatus.CREATED).build() : ResponseEntity.badRequest().build();
     }
 
     @PostMapping("#{AuthPropertiesBean.route.login}")
-    public Token login(AuthDetails authDetails) {
+    public Token login(@RequestBody AuthDetails authDetails) {
         return service.login(authDetails);
     }
 
