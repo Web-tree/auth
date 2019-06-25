e4f691090a1178e562d70277150b186ff11cc620
Max Levitskiy <max.dstu@gmail.com>
2019-06-25 13:49:59 +0200

    auth-76 Authorizer lambda

--- serverless/authorizer/handler.js
+++ serverless/authorizer/handler.js
@@ -3,24 +3,27 @@ const https = require('https');
 
 module.exports.authorize = async (event, context, callback) => {
   return new Promise((resolve, reject) => {
-    console.log("qwe")
-    const token = 'asdasd';
+    const token = event.authorizationToken;
+    console.log(token);
     const options = {
       hostname: 'auth.webtree.org',
       port: 443,
       path: '/rest/checkToken',
-      method: 'GET',
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(token)
+        'Content-Type': 'application/json'
       }
     };
 
     let request = https.request(options, res =>{
       console.log(res.statusCode);
-      res.on('data', data => {
-        console.log("data: "+data);
-        context.succeed();
+      console.log(res.statusMessage);
+      res.on('data', user => {
+        if (res.statusCode === 200) {
+          resolve(generatePolicy(JSON.parse(user), 'Allow', event.methodArn));
+        } else {
+          reject(JSON.parse(user));
+        }
       });
     });
     request.on('error', e => reject(e.message));
@@ -28,4 +31,27 @@ module.exports.authorize = async (event, context, callback) => {
     request.write(token);
     request.end();
   });
+};
+
+const generatePolicy = function(user, effect, resource) {
+  let authResponse = {};
+
+  //authResponse.principalId = user.username;
+  if (effect && resource) {
+    let policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    let statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+
+  // Optional output with custom properties of the String, Number or Boolean type.
+  authResponse.context = {
+    "username": user.username
+  };
+  return authResponse;
 };
